c9ac2dae2ad5266e2eb73633667d493588537d8e
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2024-01-02 11:20:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+function outerFunction() {
+    console.log("Entering outerFunction");
+    middleFunction();
+    console.log("Leaving outerFunction");
+}
+
+function middleFunction() {
+    console.log("Entering middleFunction");
+    innerFunction();
+    console.log("Leaving middleFunction");
+}
+
+function innerFunction() {
+    console.log("Entering innerFunction");
+    console.log("Leaving innerFunction");
+}
+
+console.log("Start of the program");
+outerFunction();
+console.log("End of the program");
